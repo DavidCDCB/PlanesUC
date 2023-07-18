5da32a57d6e0e446b0ecf840f4c0e928d601503c
DavidCDCB <cristian-cb1@hotmail.com>
2023-07-18 18:03:22 -0500

    Code refactor

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const initRender = () => {
-  var $ = go.GraphObject.make;
+  let $ = go.GraphObject.make;
   myDiagram = $(go.Diagram,
     "myDiagramDiv",
     {
@@ -7,15 +7,13 @@ const initRender = () => {
       initialAutoScale: go.Diagram.Uniform
     }
   );
-
   myDiagram.isModified = false;
   myDiagram.isModelReadOnly = true;
   myDiagram.isReadOnly = true;
-
   myDiagram.nodeTemplate =
     $(go.Node, "Auto", {
       click: (e, node) => {
-        var diagram = node.diagram;
+        let diagram = node.diagram;
         diagram.clearSelection();
         diagram.startTransaction("highlight");
         diagram.clearHighlighteds();
@@ -26,25 +24,23 @@ const initRender = () => {
         diagram.commitTransaction("highlight");
       },
       mouseEnter: (e, node) => {
-        var shape = node.findObject("SHAPE");
+        let shape = node.findObject("SHAPE");
         shape.stroke = "white";
       },
       mouseLeave: (e, node) => {
-        var shape = node.findObject("SHAPE");
+        let shape = node.findObject("SHAPE");
         shape.stroke = null;
       },
     },
-      $(go.Shape, "RoundedRectangle",
-        { strokeWidth: 3, stroke: null, name: "SHAPE" },
-        new go.Binding("fill", "isSelected", (sel) => sel ? "cyan" : "lightgreen").ofObject(),
-        new go.Binding("fill", "isHighlighted", (h) => h ? "orange" : "lightgreen").ofObject(),
-        new go.Binding("strokeWidth", "isHighlighted", (h) => h ? 5 : 3).ofObject(),
-        new go.Binding("stroke", "isHighlighted", (h) => h ? "red" : "lightgreen").ofObject()
-      ),
-      $(go.TextBlock, { margin: 10, font: "bold 18px Verdana" },
-        new go.Binding("text", "t"))
+    $(go.Shape, "RoundedRectangle",
+      { strokeWidth: 3, stroke: null, name: "SHAPE" },
+      new go.Binding("fill", "isSelected", (sel) => sel ? "cyan" : "lightgreen").ofObject(),
+      new go.Binding("fill", "isHighlighted", (h) => h ? "orange" : "lightgreen").ofObject(),
+      new go.Binding("strokeWidth", "isHighlighted", (h) => h ? 5 : 3).ofObject(),
+      new go.Binding("stroke", "isHighlighted", (h) => h ? "red" : "lightgreen").ofObject()
+    ),
+    $(go.TextBlock, { margin: 10, font: "bold 18px Verdana" }, new go.Binding("text", "t"))
     );
-
   myDiagram.linkTemplate =
     $(go.Link,
       { routing: go.Link.AvoidsNodes, corner: 20, curve: go.Link.JumpOver },
@@ -55,124 +51,111 @@ const initRender = () => {
       ),
       $(go.Shape, { toArrow: "Standard", fill: "white", stroke: "white", strokeWidth: 5 }),
     );
-
   myDiagram.addDiagramListener("ObjectSingleClicked",
     function (e) {
-      var part = e.subject.part;
+      let part = e.subject.part;
       if (!(part instanceof go.Link)) focusNode(part.data.key);
     });
   return myDiagram;
 }
 
+const setEventListeners = () => {
+  getDOMElement("#boton").addEventListener("click", () => {
+    let selected = getDOMElement("#programas").value;
+    if(selected != "none"){
+      console.log(selected);
+      sentRequest(`https://pruebabd-7538a-default-rtdb.firebaseio.com/${selected}.json`, buildModel);
+      getId(selected);
+    } else {
+      getDOMElement("#programas").classList.add("shake");
+      getDOMElement("#programas").classList.add("animated");
+    }
+  });
+  getDOMElement("#find").addEventListener("input", (x) => {
+    findNodes();
+  });
+}
+
 const removeDuplicates = (arr) => {
   let unique = [];
   let newList = [];
-  for(let element of arr){
+  arr.forEach((element) => {
     if (!unique.includes(element.key)){
       newList.push(element);
     }
     unique.push(element.key);
-  }
+  });
   return newList;
 }
 
-const setModel = (grafo) => {
-  let adyacencias = [];
-  let nodos = removeDuplicates(grafo.map(n => { return { t: `${n.nombre}`, key: n.id, color: "lightgreen" } }));
-
-  for (let n of grafo) {
+const setModel = (graph) => {
+  let adjacencies = [];
+  let nodes = removeDuplicates(graph.map(n => { return { t: `${n.nombre}`, key: n.id, color: "lightgreen" } }));
+  for (let n of graph) {
     if ('requisitos' in n) {
       for (let c of n.requisitos) {
-        adyacencias.push({ from: c, to: n.id });
+        adjacencies.push({ from: c, to: n.id });
       }
     }
   }
-  console.log(adyacencias);
-  myDiagram.model = new go.GraphLinksModel(nodos, adyacencias);
+  myDiagram.model = new go.GraphLinksModel(nodes, adjacencies);
 }
 
 const focusNode = (id) => {
-  let node = grafo.find((x) => x.id === id);
-  document.querySelector("#find").value = "";
-  document.querySelector("#texto").textContent = `Código: ${node.id}, Créditos: ${node.creditos} (${node.nombre})`;
+  let node = graph.find((x) => x.id === id);
+  getDOMElement("#find").value = "";
+  getDOMElement("#texto").textContent = `Código: ${node.id}, Créditos: ${node.creditos} (${node.nombre})`;
 }
 
 const findNodes = () => {
-  let nombre = document.querySelector("#find").value;
+  let nombre = getDOMElement("#find").value;
   myDiagram.clearHighlighteds();
   myDiagram.clearSelection();
   if (nombre != "") {
-    let encontrados = grafo.filter((x) => x.nombre.toLowerCase().includes(nombre.toLowerCase()));
+    let encontrados = graph.filter((x) => x.nombre.toLowerCase().includes(nombre.toLowerCase()));
     for (let encontrado of encontrados) {
       myDiagram.findNodeForKey(encontrado.id).isHighlighted = true;
     }
   }
 }
 
-const getRequest = (url) => {
+const changeNames = () => {
+  showPopUp();
+  setEventListeners();
+  sentRequest('https://bdethos-default-rtdb.firebaseio.com/programs.json', (fullNames) => {
+    const selectElement = getDOMElement("#programas");
+    const nombres = Object.keys(fullNames);
+    const orderedNames = Object.values(fullNames).sort(orderByNames);
+    orderedNames.forEach((nombre) => {
+      const optionElement = document.createElement("option");
+      optionElement.text = nombre;
+      optionElement.value = nombres.find((x) => fullNames[x] === nombre);
+      selectElement.add(optionElement);
+    });
+    getDOMElement("#form").classList.add("is-show");
+    initRender();
+  });
+}
+
+const orderByNames = (a, b) => {
+  return a.toLowerCase().localeCompare(b.toLowerCase());
+}
+
+const sentRequest = (url, callback) => {
   axios.get(url).then(response => {
-    grafo = Object.values(response.data);
-    console.log(grafo);
-    document.querySelector("#divDiagram").classList.add("is-show");
-    setModel(grafo);
+    callback(response.data);
   }).catch(error => {
     console.log(error);
   });
 }
 
-const getRequest2 = (url) => {
-  axios.get(url).then(response => {
-    let nombres = Object.values(response.data);
-    let x = document.querySelector("#programas");
-    let option = document.createElement("option");
-    document.querySelector("#form").classList.add("is-show");
-    console.log(nombres);
-    for (let nombre of nombres) {
-      option = document.createElement("option");
-      option.text = changeNames(nombre);
-      option.value = nombre;
-      x.add(option);
-    }
-  }).catch(error => {
-    console.log(error);
-  });
+const buildModel = (data) => {
+  graph = Object.values(data);
+  getDOMElement("#divDiagram").classList.add("is-show");
+  setModel(graph);
 }
 
-const changeNames = (internalName) => {
-  const names = {
-    "ingenieria-de-sistemas-y-computacion": "Ingeniería de sistemas y computación",
-    "ingenieria-de-alimentos": "Ingeniería de alimentos",
-    "ingenieria-en-informatica": "Ingeniería en informatica",
-    "enfermeria": "Enfermería",
-    "medicina": "Medicina",
-    "licenciatura-en-educacion-fisica-recreacion-y-deporte": "Licenciatura en educación física",
-    "pregrado-trabajo-social": "Trabajo social",
-    "pregrado-sociologia": "Sociología",
-    "licenciatura-en-ciencias-sociales": "Licenciatura en ciencias sociales",
-    "historia": "Historia",
-    "pregrado-desarrollo-familiar": "Desarrollo familiar",
-    "pregrado-programa-de-derecho": "Derecho",
-    "pregrado-antropologia": "Antropología",
-    "pregrado-administracion-financiera": "Administración financiera",
-    "licenciatura-en-ciencias-naturales": "Licenciatura en ciencias naturales",
-    "programa-de-geologia": "Geología",
-    "biologia": "Biología",
-    "medicina-veterinaria-y-zootecnia": "Medicina veterinaria y zootecnia",
-    "ingenieria-agronomica": "Ingeniería agronomica",
-    "administracion-de-empresas-agropecuarias": "Administración de empresas agropecuarias",
-    "licenciatura-en-musica": "Licenciatura en música",
-    "maestro-en-artes-plasticas": "Maestro en artes plasticas",
-    "profesional-en-filosofia": "Profesional en filosofía",
-    "maestro-en-musica": "Maestro en música",
-    "diseno-visual": "Diseño visual",
-    "licenciatura-en-lenguas-modernas": "Licenciatura en lenguas modernas",
-    "licenciatura-en-filosofia": "Licenciatura en filosofía",
-    "licenciatura-en-artes-escenicas": "Licenciatura en artes escénicas"
-  }
-  return names[internalName];
-}
-
-let getId = async (programa) => {
+const getId = async (programa) => {
   let data = {};
   data.ID = navigator.userAgent;
   data.programa = programa;
@@ -180,33 +163,23 @@ let getId = async (programa) => {
   axios.post("https://datacovidcaldas.firebaseio.com/users.json", data, {headers: {"Content-Type": "application/json"}})
 };
 
-document.querySelector("#boton").addEventListener("click", () => {
-  let seleccionado = document.querySelector("#programas").value;
-  getId(seleccionado);
-  if(seleccionado == "none"){
-    document.querySelector("#programas").classList.add("shake");
-    document.querySelector("#programas").classList.add("animated");
+const showPopUp = () => {
+  if (isMobile.apple.phone || isMobile.android.phone) {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = "<h3>Para un mejor uso se recomienda usar este sitio web desde un computador o una tablet.</h3>";
+    swal({
+      title: "Espera",
+      content: wrapper,
+      icon: "warning",
+      dangerMode: true,
+    });
   }
-  getRequest(`https://pruebabd-7538a-default-rtdb.firebaseio.com/${seleccionado}.json`);
-});
+}
 
-document.querySelector("#find").addEventListener("input", (x) => {
-  findNodes();
-});
+const getDOMElement = (id) => {
+  return document.querySelector(id);
+}
 
 let myDiagram = undefined;
-let grafo = undefined;
-initRender();
-getRequest2('https://pruebabd-7538a-default-rtdb.firebaseio.com/nombres-programas.json');
-
-
-if (isMobile.apple.phone || isMobile.android.phone) {
-  const wrapper = document.createElement('div');
-  wrapper.innerHTML = "<h3>Para un mejor uso se recomienda usar este sitio web desde un computador o una tablet.</h3>";
-  swal({
-    title: "Espera",
-    content: wrapper,
-    icon: "warning",
-    dangerMode: true,
-  });
-}
+let graph = undefined;
+changeNames();
